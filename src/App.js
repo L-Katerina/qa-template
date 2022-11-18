1c279c59c84f3395014958cda53f27f4a5927fee
Fusion <fusion@Fusions-Mac-mini.local>
2022-11-18 17:38:10 +0300

    add src/App.css src/App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='head'>
-          <div className='logo1'>
-            <div className='logo2'>QA LOGO</div>
+        <div className="head">
+          <div className="logo1">
+            <div className="logo2">QA LOGO</div>
           </div>
-          <div className='search'>
-            <div className='button'>SEARSH</div>
+          <div className="search">
+            <div className="button">SEARSH</div>
           </div>
         </div>
-        <nav className='menu'>
-          <a className='home' href="#">HOME</a>
+        <nav className="menu">
+          <a className="home" href="#">
+            HOME
+          </a>
           <a href="#">ABOUT US</a>
           <a href="#">SERVICE</a>
           <a href="#">PARTNERS</a>
           <a href="#">CUSTOMERS</a>
           <a href="#">PROGECT</a>
           <a href="#">CAREERS</a>
-          <a className='contact' href="#">CONTACT</a>
+          <a className="contact" href="#">
+            CONTACT
+          </a>
         </nav>
       </header>
+      <body>
+        <div className="item">
+          <h2>Aliquam pharetra vulputate</h2>
+          <div className="content">
+            <p>
+              Vivamus est mauris, sollicitudin ut ipsum pharetra, porta rutrum
+              turpis. Suspendisse sollicitudin quam eget condimentum posuere. In
+              auctor vel mi porta commodo. Vestibulum mattis fringilla tempus.
+              Etiam eu consectetur nisi. Aenean blandit sodales euismod. In hac
+              habitasse platea dictumst.
+            </p>
+            <div className="more">READ MORE</div>
+          </div>
+        </div>
+        <div className="post">
+          <div className="part1">
+            <div className="postleft">
+              <div className="postname">
+                <div id="pink" className="dot1"></div>
+                <div className="txt">ABOUT SUPER LOGO</div>
+                <div id="pink" className="line1"></div>
+              </div>
+              <div>
+                <img src="https://i.ibb.co/x3yv0p6/download.png"></img>
+                <p className="lorem">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  quam massa, dignissim at nisi nec, iaculis molestie leo.
+                  Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                  sem a efficitur auctor. Sed vulputate vitae dui non fermentum.
+                  Donec non sem dignissim, tincidunt arcu eget, posuere tellus.
+                  Quisque consectetur diam id faucibus sagittis. Praesent
+                  placerat, diam non sollicitudin suscipit, eros erat
+                </p>
+                <p className="link">Reade more...</p>
+              </div>
+            </div>
+            <div className="postright">
+              <div className="postname">
+                <div id="blue" className="dot1"></div>
+                <div className="txt">SOME WORDS OUR CEO</div>
+                <div id="blue" className="line2"></div>
+              </div>
+              <div>
+                <img src="https://i.ibb.co/0Cjk9jF/download-1.png"></img>
+                <p className="lorem">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  quam massa, dignissim at nisi nec, iaculis molestie leo.
+                  Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                  sem a efficitur auctor. Sed vulputate vitae dui non fermentum.
+                  Donec non sem dignissim, tincidunt arcu eget, posuere tellus.
+                  Quisque consectetur diam id faucibus sagittis. Praesent
+                  placerat, diam non sollicitudin suscipit, eros erat
+                </p>
+                <p className="link">Steve Jobs, CEO</p>
+              </div>
+            </div>
+          </div>
+          <div className="part2">
+            <div className="postleft">
+              <div className="postname">
+                <div id="blue" className="dot1"></div>
+                <div className="txt">ABOUT SUPER LOGO</div>
+                <div id="blue" className="line1"></div>
+              </div>
+              <div>
+                <img src="https://i.ibb.co/S50RWwQ/download-2.png"></img>
+                <p className="lorem">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  quam massa, dignissim at nisi nec, iaculis molestie leo.
+                  Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                  sem a efficitur auctor. Sed vulputate vitae dui non fermentum.
+                  Donec non sem dignissim, tincidunt arcu eget, posuere tellus.
+                  Quisque consectetur diam id faucibus sagittis. Praesent
+                  placerat, diam non sollicitudin suscipit, eros erat
+                </p>
+                <p className="link decor">Read more...</p>
+              </div>
+            </div>
+            <div className="postright">
+              <div className="postname">
+                <div id="pink" className="dot1"></div>
+                <div className="txt">SOME WORDS OUR CEO</div>
+                <div id="pink" className="line2"></div>
+              </div>
+              <div>
+                <img src="https://i.ibb.co/Hzf4YR6/download-1-2.png"></img>
+                <p className="lorem">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  quam massa, dignissim at nisi nec, iaculis molestie leo.
+                  Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                  sem a efficitur auctor. Sed vulputate vitae dui non fermentum.
+                  Donec non sem dignissim, tincidunt arcu eget, posuere tellus.
+                  Quisque consectetur diam id faucibus sagittis. Praesent
+                  placerat, diam non sollicitudin suscipit, eros erat
+                </p>
+                <p className="link">Steve Jobs, CEO</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="blocks">
+          <div className="left">
+            <div className="lmr">
+              <div id="blue" className="dot1"></div>
+              <div className="txt">SERVICES</div>
+              <div id="blue" className="line4"></div>
+            </div>
+            <p className="services">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam
+              massa, dignissim at nisi nec, iaculis molestie leo. Aenean
+              hendrerit pretium enim quis rutrum. Pellentesque mattis sem a
+              efficitur auctor. Sed vulputate vitae dui non fermentum. Donec non
+              sem dignissim, tincidunt arcu eget, posuere tellus. Quisque
+              consectetur diam id faucibus sagittis. Praesent placerat, diam non
+              sollicitudin suscipit, eros erat
+            </p>
+            <p className="link2 decor">Read more...</p>
+          </div>
+          <div className="middle">
+            <div className="lmr">
+              <div id="pink" className="dot3"></div>
+              <div className="txt">RESENT POSTS</div>
+              <div id="pink" className="line5"></div>
+            </div>
+            <div id="description" className="car">
+              <img className="mini" src="https://i.ibb.co/9rS8M2v/car.png"></img>
+              <p>LOREM IPSUM</p>
+              <p>
+                Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                sem a efficitur auctor.
+              </p>
+            </div>
+            <div id="description" className="phone">
+              <img className="mini" src="https://i.ibb.co/yn55bYv/phone.png"></img>
+              <p>LOREM IPSUM</p>
+              <p>
+                Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                sem a efficitur auctor.
+              </p>
+            </div>
+            <div id="description" className="bbc">
+              <img className="mini" src="https://i.ibb.co/q5YFFTp/bbc-news.png"></img>
+              <p>LOREM IPSUM</p>
+              <p>
+                Aenean hendrerit pretium enim quis rutrum. Pellentesque mattis
+                sem a efficitur auctor.
+              </p>
+            </div>
+          </div>
+          <div className="right">
+            <div className="lmr">
+              <div id="blue" className="dot3"></div>
+              <div className="txt">SERVICES</div>
+              <div id="blue" className="line4"></div>
+            </div>
+            <p className="services">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam
+              massa, dignissim at nisi nec, iaculis molestie leo. Aenean
+              hendrerit pretium enim quis rutrum. Pellentesque mattis sem a
+              efficitur auctor. Sed vulputate vitae dui non fermentum. Donec non
+              sem dignissim, tincidunt arcu eget, posuere tellus. Quisque
+              consectetur diam id faucibus sagittis. Praesent placerat, diam non
+              sollicitudin suscipit, eros erat
+            </p>
+            <p className="link2 decor">Read more...</p>
+          </div>
+        </div>
+        <div className="clients">
+          <div className="topline">
+            <div id="pink" className="topdot"></div>
+            <div>CLIENTS</div>
+            <div id="pink" className="linetop"></div>
+          </div>
+          <div className="logoclient">
+            <img src="https://i.ibb.co/ysPJnnz/client-1-1.png"></img>
+            <img src="https://i.ibb.co/1QhqRFP/client-2-1.png"></img>
+            <img src="https://i.ibb.co/dQWBr3N/client-3-1.png"></img>
+            <img src="https://i.ibb.co/4dkhnMZ/client-5-1.png"></img>
+            <img src="https://i.ibb.co/ypv37Pc/client-4-1.png"></img>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
